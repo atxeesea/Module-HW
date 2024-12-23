7426f3e6cbe6f2b1d0a8b56abc6fef6a5f937558
atxeesea <sadchikov.andrei06@gmail.com>
2024-12-23 18:53:59 +0300

    Add files via upload

--- shopping_list.py
+++ shopping_list.py
@@ -1,4 +1,22 @@
-items = []
+shopping_list = {}
 
-def add_item(name):
-    return items.append(name)
+def add_item(item, quantity=0):
+    if item in shopping_list:
+        shopping_list[item] += quantity
+    else:
+        shopping_list[item] = quantity
+
+def remove_item(item):
+    if item in shopping_list:
+        del shopping_list[item]
+    else:
+        print(f"Товар '{item}' не найден в списке.")
+
+def edit_quantity(item, quantity):
+    if item in shopping_list:
+        shopping_list[item] = quantity
+    else:
+        print(f"Товар '{item}' не найден в спиксе.")
+
+def view_list():
+    return shopping_list
